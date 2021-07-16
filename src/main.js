b17c1bf356f93b73eae51abea0f8d3b02f322238
elbana-fci <elbana.fci@gmail.com>
2021-07-16 10:42:58 +0200

    Created Components

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './routes/index';
+import firebase from 'firebase';
+import store from './store';
 
-createApp(App).mount('#app')
+
+
+//Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAi3WKDH2YHf42IGyAPWlnzouDg3ulZPcE",
+  authDomain: "plain-api.firebaseapp.com",
+  projectId: "plain-api",
+  storageBucket: "plain-api.appspot.com",
+  messagingSenderId: "309014286089",
+  appId: "1:309014286089:web:8aedbd70d03ff229f90dd8",
+  measurementId: "G-0CWWF05G5D"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(user => {
+    store.dispatch("fetchUser", user);
+});
+
+createApp(App).use(store).use(router).mount("#app");
