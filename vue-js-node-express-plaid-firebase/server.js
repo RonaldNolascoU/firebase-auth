290ea752f6efb184a72529b95397e2745a1f3a78
RonaldNolascoU <alexnolasco_2010@hotmail.com>
2021-07-28 10:28:45 -0600

    plaid vercel

--- vue-js-node-express-plaid-firebase/server.js
+++ vue-js-node-express-plaid-firebase/server.js
@@ -1,11 +1,11 @@
 //require('dotenv').config();
-require('dotenv/config');
-const crypto = require("crypto");
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const firebase = require('./db');
-const firestore = firebase.firestore();
+require('dotenv/config')
+const crypto = require('crypto')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const firebase = require('./db')
+const firestore = firebase.firestore()
 //const history = require('connect-history-api-fallback');
 /* The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
@@ -14,66 +14,69 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();*/
 
-const path = __dirname + '/app/views/';
+const path = __dirname + '/app/views/'
 
-const app = express();
+const app = express()
 
-app.use(express.static(path));
+app.use(express.static(path))
 
 var corsOptions = {
-  origin: "http://localhost:8080"
-};
+  origin: '*',
+}
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 /*app.use(history());*/
 
-const util = require('util');
+const util = require('util')
 
-const plaid = require('plaid');
+const plaid = require('plaid')
 const plaidClient = new plaid.Client({
-    clientID: '60f32ab0219f3b0012a29fee',
-    secret: 'f65b67aa0d3acddd65a4e0df9ff0b0',
-    env: plaid.environments.sandbox,
-    options: {
-      version: "2020-09-14",
-    },
-});
+  clientID: '60f32ab0219f3b0012a29fee',
+  secret: 'f65b67aa0d3acddd65a4e0df9ff0b0',
+  env: plaid.environments.sandbox,
+  options: {
+    version: '2020-09-14',
+  },
+})
 
-app.post('/register', function (req, res) {
+app.post('/register', function(req, res) {
   firebase
     .auth()
     .createUserWithEmailAndPassword(req.body.email, req.body.password)
-    .then(data => {
+    .then((data) => {
       data.user
         .updateProfile({
-          displayName: req.body.name
+          displayName: req.body.name,
         })
-        .then(() => { console.log('User Registered Successfully!');});
+        .then(() => {
+          console.log('User Registered Successfully!')
+        })
     })
-    .catch(err => {
-      console.log('User not Registered!');
-    });
+    .catch((err) => {
+      console.log('User not Registered!')
+    })
   /*res.send({
     message: `User Registered Successfully ${req.body.email}`,
   })*/
 })
 
-app.post('/login', function (req, res) {
+app.post('/login', function(req, res) {
   firebase
     .auth()
     .signInWithEmailAndPassword(req.body.email, req.body.password)
     .then(() => {
-      console.log('User Logged Successfully!');
+      console.log('User Logged Successfully!')
     })
-    .catch(err => {
-      console.log('Bad Login!');
-    });
+    .catch((err) => {
+      console.log('Bad Login!')
+    })
 })
 
-app.post('/link/token/create', async (req,res) => {
-    const response = await plaidClient.createLinkToken({
+app.post('/link/token/create', async (req, res) => {
+  const response = await plaidClient
+    .createLinkToken({
       user: {
         client_user_id: '123-test-user-id',
       },
@@ -82,52 +85,53 @@ app.post('/link/token/create', async (req,res) => {
       country_codes: ['GB'],
       language: 'en',
       webhook: 'https://sample-web-hook.com',
-      
     })
     .catch((err) => {
-      console.log(err);
-    });
-  const linkToken = response.link_token;
-  console.log(linkToken);
-  res.json({linkToken});
-     
-});
+      console.log(err)
+    })
+  const linkToken = response.link_token
+  console.log(linkToken)
+  res.json({ linkToken })
+})
 
-app.post('/item/public_token/exchange', async (req,res) => {
-    const { publicToken } = req.body;
-    const { access_token: accessToken } = await plaidClient.exchangePublicToken(publicToken);
+app.post('/item/public_token/exchange', async (req, res) => {
+  const { publicToken } = req.body
+  const { access_token: accessToken } = await plaidClient.exchangePublicToken(
+    publicToken
+  )
 
-    const authResponse = await plaidClient.getAuth(accessToken);
-    console.log('-----------');
-    console.log('Auth response');
-    //const authdata = util.inspect(authResponse, false, null, true);
-    console.log(authResponse);
+  const authResponse = await plaidClient.getAuth(accessToken)
+  console.log('-----------')
+  console.log('Auth response')
+  //const authdata = util.inspect(authResponse, false, null, true);
+  console.log(authResponse)
 
-    await firestore.collection('auth').doc().set(authResponse);
-    
-    const identityResponse = await plaidClient.getIdentity(accessToken);
-    console.log('-----------');
-    console.log('Identity response');
-    console.log(util.inspect(identityResponse, false, null, true));
+  await firestore
+    .collection('auth')
+    .doc()
+    .set(authResponse)
 
-    const balanceResponse = await plaidClient.getBalance(accessToken);
-    console.log('-----------');
-    console.log('Balance response');
-    console.log(util.inspect(balanceResponse, false, null, true));
+  const identityResponse = await plaidClient.getIdentity(accessToken)
+  console.log('-----------')
+  console.log('Identity response')
+  console.log(util.inspect(identityResponse, false, null, true))
 
-    res.sendStatus(200);
-});
+  const balanceResponse = await plaidClient.getBalance(accessToken)
+  console.log('-----------')
+  console.log('Balance response')
+  console.log(util.inspect(balanceResponse, false, null, true))
 
-app.get('/', function (req,res) {
-  res.sendFile(path + "index.html");
-});
+  res.sendStatus(200)
+})
 
-var history = require('connect-history-api-fallback');
+app.get('/', function(req, res) {
+  res.sendFile(path + 'index.html')
+})
+
+var history = require('connect-history-api-fallback')
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.use(history()).listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
-
-
+  console.log(`Server is running on port ${PORT}.`)
+})
